c84f2a9ce19b725f0bdac3e7f7611a4fb1523117
Hugo Menezes <hugomenezes@gmail.com>
2017-12-14 21:25:23 -0300

    save das entidades

--- DataSet.js
+++ DataSet.js
@@ -1,15 +1,20 @@
 class DataSet {
 
-    save(entity) {
+    constructor(){
+        this.entities = new Map();
+        this.entities.set("clients", []);
+        this.entities.set("accounts", []);
+    }
 
+    save(entity) {
+        this.entities.get("clients").push(entity.titular);
+        this.entities.get("accounts").push(entity.saldo);
     }
 
     update(entity) {
-
     }
 
     delete(entity) {
-
     }
 }
 
