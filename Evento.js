b0f863202e43339a30e5f0912936ad401eea5a84
INTRANET\fca <fernando.andrade@pitang.com>
2018-01-03 13:18:13 -0300

    Alteradas entidade para a reprodução

--- Evento.js
+++ Evento.js
@@ -1,13 +1,17 @@
 class Evento {
-    constructor(name, processName, dataDoEvento, instancia, responsavel, payload, origem, dataRef) {
+    constructor(name, processName, dataDoEvento, instancia, responsavel, payload, origem, dataRef, reproducao) {
         this.name = name;
         this.processName = processName;
+        if (!dataDoEvento) {
+            dataDoEvento = new Date();
+        }
         this.dataDoEvento = dataDoEvento;
         this.instancia = instancia;
         this.responsavel = responsavel;
         this.payload = payload;
         this.origem = origem;
         this.dataRef = dataRef;
+        this.reproducao = reproducao;
     }
 }
 
---
+++ Reproducao.js
@@ -0,0 +1,11 @@
+class Reproducao {
+    constructor(id, instanciaOriginal, instanciaReproducao, dataReproducao, responsavel) {
+        this.id = id;
+        this.instanciaOriginal = instanciaOriginal;
+        this.instanciaReproducao = instanciaReproducao;
+        this.dataReproducao = dataReproducao;
+        this.responsavel = responsavel;
+    }
+}
+
+module.exports = Reproducao;
