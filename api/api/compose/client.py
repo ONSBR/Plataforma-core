028afdfbd71b5f8a25061fde3104790d9cd95201
Bruno Leitao De Oliveira <bleitao@ons.org.br>
2018-02-26 10:10:33 -0300

    inclusao de testes da api

--- api/api/compose/client.py
+++ api/api/compose/client.py
@@ -4,8 +4,6 @@ from urllib.request import urlopen
 
 class ClientAPI(object):
     def request(self, url):
-
-        import pdb; pdb.set_trace()
         with urlopen(url) as response:
             html = response.read()
 
